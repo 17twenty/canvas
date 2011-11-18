fdf05b0a2fb9dd5e95fe79b7601bd09e0ae1d337
Ben Crundwell <bjc1@cambridge-design.co.uk>
2011-11-18 15:08:46 +0000

    Running a constant framerate to prevent video issues

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-/******************************************************************************
- *
- * Copyright (c) 2011 Cambridge Design Partnership
- * All rights reserved.
+/*******************************************************************************
+ * 
+ * Copyright (c) 2011 Cambridge Design Partnership All rights reserved.
  * 
  * This file is part of the Canvas Project
- *
- * Commercial Usage
- * Licensees holding a commercial license for the Canvas Project are provided
- * with the rights agreed when taking out this license and are bound to these
- * terms in perpetuity.
- *
- * GNU Lesser General Public License Usage
- * Alternatively, this file may be used under the terms of the GNU Lesser
- * General Public License version 2.1 as published by the Free Software
- * Foundation and appearing in the file LICENSE.LGPL included in the
- * packaging of this file.
- * Please review the following information to ensure the GNU Lesser General
- * Public License version 2.1 requirements will be met:
+ * 
+ * Commercial Usage Licensees holding a commercial license for the Canvas
+ * Project are provided with the rights agreed when taking out this license and
+ * are bound to these terms in perpetuity.
+ * 
+ * GNU Lesser General Public License Usage Alternatively, this file may be used
+ * under the terms of the GNU Lesser General Public License version 2.1 as
+ * published by the Free Software Foundation and appearing in the file
+ * LICENSE.LGPL included in the packaging of this file. Please review the
+ * following information to ensure the GNU Lesser General Public License version
+ * 2.1 requirements will be met:
  * http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
- *
+ * 
  ******************************************************************************/
 
-//CONSTANTS
+// CONSTANTS
 IMAGE = 0;
 VIDEO = 1;
 
 UPDATE = 0;
 REFRESH = 1;
 
-//GLOBALS
+// GLOBALS
 var drag_flag = false; 
 var rotate_flag = false; 
 var resize_flag = false;
@@ -61,11 +58,18 @@ var removeIcons;
 
 v = document.getElementById('v');
 
-function draw(v) {
-    if(v.paused || v.ended) return false;
-	render();
-    setTimeout(draw,50,v);
-}
+
+// function draw(v) {
+// if(v.paused || v.ended)
+// {
+// clearTimeout(videoTimeout);
+// }
+// else
+// {
+// render();
+// videoTimeout = setTimeout(draw,20,v);
+// }
+// }
 	
 function init()
 {
@@ -73,30 +77,39 @@ function init()
 	google.setOnLoadCallback(function() {
 		console.log("Start Loading fonts");
 		WebFont.load({
-		    google: {
-		      families: [ 'Loved by the King' ]
-		    },
-		    active: function(){
-		    	console.log("Font Loaded");
-		    	fontsLoaded = true;
-		  		render();
-		  	}
-		  });
+			google: {
+				families: [ 'Loved by the King' ]
+			},
+			active: function(){
+				console.log("Font Loaded");
+				fontsLoaded = true;
+				render();
+			}
+		});
 	});
 	canvas = document.getElementById("myCanvas");
 	ctx=canvas.getContext("2d");
-        resize();
-	
+	resize();
+
 	console.log("Initialisation");
-    Refresh=setInterval("ajaxFunction(0,REFRESH);",5000);
-    
-    
-	 
+	Refresh=setInterval("ajaxFunction(0,REFRESH);",10000);
+
+	// Set Framerate
+	renderTimeout = setInterval(render,50);
+	
+	canvas.addEventListener("dragover", function(e) {
+		e.preventDefault();
+		//e.dataTransfer.files.
+	}, true);
+	canvas.addEventListener("drop", function(e) {
+		e.preventDefault();
+		console.log("Dropped!");
+	}, true);
 }
 
 function CanvasImage() {
-        this.id = 0;
-        this.type  = IMAGE;
+	this.id = 0;
+	this.type  = IMAGE;
 	this.x = 0.5;
 	this.y = 0.5;
 	this.z = 0;
@@ -109,53 +122,50 @@ function CanvasImage() {
 }
 
 function addObject(id, type, x, y, z, size, rotation, desc, src) {
-    
-    console.log("Add: " + src);
-    var tempImage = new CanvasImage;
-    tempImage.id = id;
-    tempImage.type = type;
-    tempImage.x = x;
-    tempImage.y = y;
-    tempImage.z = z;
-    tempImage.size = size;
-    tempImage.rotation = rotation;
-    tempImage.desc = desc;
-    
-    if(type == VIDEO)
-        {
-        tempImage.image = document.createElement('video');
-        tempImage.image.src = src;
-        document.body.appendChild(tempImage.image);
-        var newid = objects.push(tempImage) - 1;
-        objects[newid].image.newid = newid;
-        
-//        tempImage.image = video;
-        
-        }
-    else if(type == IMAGE)
-        {
-        tempImage.image.src = src;
-        var newid = objects.push(tempImage) - 1;
-        objects[newid].image.onload = function(){
-            console.log("Image " + objects[newid].image.src + " Loaded");
-            objects[newid].loaded = true;
-            objects[newid].aspectRatio = objects[newid].image.height / objects[newid].image.width;
-            render();
-            };
-        }
+
+	console.log("Add: " + src);
+	var tempImage = new CanvasImage;
+	tempImage.id = id;
+	tempImage.type = type;
+	tempImage.x = x;
+	tempImage.y = y;
+	tempImage.z = z;
+	tempImage.size = size;
+	tempImage.rotation = rotation;
+	tempImage.desc = desc;
+
+	if(type == VIDEO)
+	{
+		tempImage.image = document.createElement('video');
+		tempImage.image.src = src;
+		document.body.appendChild(tempImage.image);
+		var newid = objects.push(tempImage) - 1;
+		objects[newid].image.newid = newid;
+	}
+	else if(type == IMAGE)
+	{
+		tempImage.image.src = src;
+		var newid = objects.push(tempImage) - 1;
+		objects[newid].image.onload = function(){
+			console.log("Image " + objects[newid].image.src + " Loaded");
+			objects[newid].loaded = true;
+			objects[newid].aspectRatio = objects[newid].image.height / objects[newid].image.width;
+			render();
+		};
+	}
 }
 
+// Workaround to render Video and get AspectRatio for Chrome
 window.addEventListener('loadedmetadata', function(e) { 
+	console.log("Video " + e.target.src + " Loaded");
+	e.target.height = e.target.videoHeight;
+	e.target.width = e.target.videoWidth;
+	objects[e.target.newid].aspectRatio = e.target.height / e.target.width; 
+	objects[e.target.newid].loaded = true;
+	render();
+}, true);
 
- //       tempImage.image.onloadedmetadata = function(e){
-            console.log("Video " + e.target.src + " Loaded");
-            e.target.height = e.target.videoHeight;
-            e.target.width = e.target.videoWidth;
-            objects[e.target.newid].aspectRatio = e.target.height / e.target.width; 
-            objects[e.target.newid].loaded = true;
-            render();
-   //         }
-            }, true);
+
 
 function resize()
 {
@@ -172,84 +182,54 @@ function convertSize(size) {return size * window.innerWidth;}
 function drawImage(image)
 {
 	var size = convertSize(image.size);
-    //console.log("drawImage " + v.width + ", " + v.height);
+    // console.log("drawImage " + v.width + ", " + v.height);
     ctx.save();  // Save co-ordinate system
     ctx.translate(image.x * window.innerWidth, image.y * window.innerHeight);
     ctx.rotate(image.rotation * Math.PI / 180);
 
-
-//    {
-//    ctx.shadowOffsetX = 0;
-//    ctx.shadowOffsetY = 0;
-//    ctx.shadowBlur    = 4;
-//    ctx.shadowColor   = 'rgba(0, 0, 0, 0.5)';    
-//    }
     if((drag_flag || rotate_flag || resize_flag) && image.z == maxz)    
-        {
-        ctx.shadowOffsetX = 10;
-        ctx.shadowOffsetY = 10;
-        ctx.shadowBlur    = 10;
-        ctx.shadowColor   = 'rgba(0, 0, 0, 0.5)';    
-        }
-    //Frame around each item
-    if (1)
-	{
-    	ctx.beginPath(); 
-	    ctx.rect(-0.5*size-10, -0.5 * size * image.aspectRatio -25, size+20, (size * image.aspectRatio)+50);	
-	    ctx.closePath(); 
-	    //ctx.lineWidth = 10;
-	    ctx.fillStyle = "#FFFFFF"; 
-	    //ctx.strokeStyle = "#FFFFFF"; 
-	    //ctx.lineCap = "square";
-	    //ctx.stroke(); 
-	    ctx.fill();
-
-	    ctx.shadowOffsetX = 0;
-	    ctx.shadowOffsetY = 0;
-	    ctx.shadowBlur    = 0;
-	    ctx.shadowColor   = null;    
-	    
-	    if(fontsLoaded)
-	    {ctx.fillStyle = "#000000"; 
-	    ctx.font = "20px 'Loved by the King', cursive";
-	    ctx.textBaseline = "middle";
-	    ctx.textAlign = "center";
-	    ctx.fillText(image.desc, 0, 0.5*size*image.aspectRatio + 5, size);}
-	}
-    else
     {
-    	ctx.beginPath(); 
-	    ctx.rect(-0.5*size-5, -0.5*size * image.aspectRatio-5, size+10, (size * image.aspectRatio)+10);	
-	    ctx.closePath(); 
-	    ctx.lineWidth = 10;
-	    ctx.strokeStyle = "#FFFFFF"; 
-	    ctx.lineCap = "square";
-	    ctx.stroke(); 
+    	ctx.shadowOffsetX = 10;
+    	ctx.shadowOffsetY = 10;
+    	ctx.shadowBlur    = 10;
+    	ctx.shadowColor   = 'rgba(0, 0, 0, 0.5)';    
     }
     
-    
+    // Frame around each item
+    ctx.beginPath(); 
+    ctx.rect(-0.5*size-10, -0.5 * size * image.aspectRatio -25, size+20, (size * image.aspectRatio)+50);	
+    ctx.fillStyle = "#FFFFFF"; 
+    ctx.fill();
+
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.shadowBlur    = 0;
+    ctx.shadowColor   = null;    
+
+    if(fontsLoaded)
     {
-        ctx.shadowOffsetX = 0;
-        ctx.shadowOffsetY = 0;
-        ctx.shadowBlur    = 0;
-        ctx.shadowColor   = null;    
+    	ctx.fillStyle = "#000000"; 
+    	ctx.font = "20px 'Loved by the King', cursive";
+    	ctx.textBaseline = "middle";
+    	ctx.textAlign = "center";
+    	ctx.fillText(image.desc, 0, 0.5*size*image.aspectRatio + 5, size);
     }
 
     ctx.drawImage(image.image, -0.5*size, -0.5*size * image.aspectRatio - 15, size, (size * image.aspectRatio));
 
     if (VIDEO == image.type && (image.image.paused ||  image.image.ended))
-    	{
+    {
     	ctx.beginPath(); 
-	    ctx.moveTo(-0.1*size,-0.1*size-15);
-	    ctx.lineTo(-0.1*size, 0.1*size-15);
-	    ctx.lineTo(0.1*size, -15);
-	    ctx.closePath(); 
-	    ctx.lineWidth = 10;
-	    ctx.strokeStyle = "#FFFFFF";  
-	    ctx.globalAlpha = 0.2;  
-	    ctx.lineJoin = 'round';
-	    ctx.stroke(); 
-    	}
+    	ctx.moveTo(-0.1*size,-0.1*size-15);
+    	ctx.lineTo(-0.1*size, 0.1*size-15);
+    	ctx.lineTo(0.1*size, -15);
+    	ctx.closePath(); 
+    	ctx.lineWidth = 10;
+    	ctx.strokeStyle = "#FFFFFF";  
+    	ctx.globalAlpha = 0.2;  
+    	ctx.lineJoin = 'round';
+    	ctx.stroke(); 
+    }
 
     ctx.restore();  // Restore co-ordinate system
 }
@@ -270,6 +250,7 @@ function myDown(e){
 	var l = objects.length;
     for (var i = l-1; i >= 0; i--)
 	{
+    	
         var angle_to_TR = clean_angle_rad(Math.atan2(-1 * (convertSize(objects[i].size) * objects[i].aspectRatio + 30), convertSize(objects[i].size)) + (objects[i].rotation * Math.PI  / 180));
         var angle_to_BR = clean_angle_rad(Math.atan2((convertSize(objects[i].size) * objects[i].aspectRatio + 30), convertSize(objects[i].size)) + (objects[i].rotation * Math.PI  / 180));
         var angle_to_BL = clean_angle_rad(angle_to_TR + Math.PI);
@@ -287,7 +268,7 @@ function myDown(e){
 		var BR_BL = clean_angle(objects[i].rotation + 180);
 		var BL_TL = clean_angle(objects[i].rotation + 270);
 			
-		//Rotate hotspot
+		// Rotate hotspot
 		if (    objects[i].loaded && 
 				(  e.pageX < objects[i].x * window.innerWidth + Math.cos(angle_to_TR)  * mag_to_corner + hotspot_size/2 
                         && e.pageX > objects[i].x * window.innerWidth + Math.cos(angle_to_TR)  * mag_to_corner - hotspot_size/2
@@ -311,15 +292,14 @@ function myDown(e){
 			temp_y = (e.pageY);
 			resise_ratio = objects[i].size / (Math.sqrt(Math.pow((e.pageY - (objects[i].y * window.innerHeight)),2) + Math.pow((e.pageX - (objects[i].x * window.innerWidth)),2) ));
 			rotate_offset = Math.atan2((temp_y - (objects[i].y * window.innerHeight)) , (temp_x - (objects[i].x * window.innerWidth))) * 180 / Math.PI - objects[i].rotation;
-                        rotate_flag = true;
-			resize_flag = true;
+            rotate_flag = true;
 			imageId = i;
 			canvas.onmousemove = myMove;
 			displayIcons = true;
 			if (removeIcons != null) clearTimeout(removeIcons);
 			return;
 		}
-		//Move hotspot
+		// Move hotspot
 		else if ((clean_angle(cursor_TL - TL_TR) > 0) && (clean_angle(cursor_TR - TR_BR) > 0) && (clean_angle(cursor_BR - BR_BL) > 0) && (clean_angle(cursor_BL - BL_TL) > 0)) 
 		{
 			if (objects[i].z < maxz) {
@@ -345,7 +325,7 @@ function myDown(e){
 }
 
 function myMove(e){
-        moved_flag = true;
+    moved_flag = true;
 	if (drag_flag)	{
 		objects[imageId].x = (e.pageX + temp_x) / window.innerWidth;
 		objects[imageId].y = (e.pageY + temp_y) / window.innerHeight;
@@ -353,40 +333,38 @@ function myMove(e){
 	}
 	if (rotate_flag)	{
 		objects[imageId].rotation = (Math.atan2((e.pageY - (objects[imageId].y * window.innerHeight)) , (e.pageX - (objects[imageId].x * window.innerWidth))) * 180 / Math.PI - rotate_offset);
-		render();
-	}
-	if (resize_flag)	{
 		objects[imageId].size = (Math.sqrt(Math.pow((e.pageY - (objects[imageId].y * window.innerHeight)),2) + Math.pow((e.pageX - (objects[imageId].x * window.innerWidth)),2)) * resise_ratio );
 		if (objects[imageId].size < 0.05) objects[imageId].size = 0.05;
-                render();
+		// render(); //Fixing the processor load issue
 	}
 }
 
 function myUp(){
 	canvas.onmousemove = null;
-        removeIcons=setTimeout("displayIcons = false;",3000);
+    removeIcons=setTimeout("displayIcons = false;",3000);
 	if (moved_flag == false && drag_flag) {
-            if (objects[imageId].type == VIDEO) {
-		console.log("Play Video");
-		if(objects[imageId].image.paused ||  objects[imageId].image.ended) objects[imageId].image.play(); else objects[imageId].image.pause(); 
-		
-		draw(objects[imageId].image);
-            }
+		if (objects[imageId].type == VIDEO) {
+			console.log("Play Video");
+			if(objects[imageId].image.paused ||  objects[imageId].image.ended) objects[imageId].image.play(); else objects[imageId].image.pause(); 
+
+			// draw(objects[imageId].image);
+		}
 	}
-        else
-            {
-            //console.log(sequence);
-            ajaxFunction(imageId, UPDATE);
-            }
+	else
+	{
+		// console.log(sequence);
+		ajaxFunction(imageId, UPDATE);
+	}
 	moved_flag = false;
 	drag_flag = false;
 	rotate_flag = false;
 	resize_flag = false;
-        render();
+        // render();
+	
 }
 function sortNumber(a,b)
 {
-    //console.log(a.z + " - " + b.z);
+    // console.log(a.z + " - " + b.z);
     return a.z - b.z;
 }
 
@@ -400,13 +378,17 @@ function render()
     for (var i = 0; i < l; i++) 
 	{
         if (objects[i].z > maxz) maxz = objects[i].z;
-        //if (objects[i].type == VIDEO) objects[i].loaded =  (objects[i].image.readyState > 0);
+        // if (objects[i].type == VIDEO) objects[i].loaded =
+		// (objects[i].image.readyState > 0);
         if (objects[i].loaded)
             {
             drawImage(objects[i]);
-//            console.log(Math.atan2(-1 * convertSize(objects[i].size) * objects[i].aspectRatio + 30, convertSize(objects[i].size)) + objects[i].rotation * Math.PI  / 180);
-//            console.log("Height: " + (-1 * convertSize(objects[i].size) * objects[i].aspectRatio + 30));
-//            console.log(convertSize(objects[i].size));
+// console.log(Math.atan2(-1 * convertSize(objects[i].size) *
+// objects[i].aspectRatio + 30, convertSize(objects[i].size)) +
+// objects[i].rotation * Math.PI / 180);
+// console.log("Height: " + (-1 * convertSize(objects[i].size) *
+// objects[i].aspectRatio + 30));
+// console.log(convertSize(objects[i].size));
             
             
             var angle_to_TR = Math.atan2(-1 * (convertSize(objects[i].size) * objects[i].aspectRatio + 30), convertSize(objects[i].size)) + objects[i].rotation * Math.PI  / 180;
@@ -429,7 +411,7 @@ function render()
 
 
 
-//Browser Support Code
+// Browser Support Code
 function ajaxFunction(id, action){
     var ajaxRequest;  // The variable that makes Ajax possible!
 
@@ -453,8 +435,8 @@ function ajaxFunction(id, action){
     // Create a function that will receive data sent from the server
     ajaxRequest.onreadystatechange = function(){
         if(ajaxRequest.readyState == 4){
-            //objects.length = 0;  
-            //render();
+            // objects.length = 0;
+            // render();
             switch(action)
             {
                 case UPDATE:
@@ -478,7 +460,7 @@ function ajaxFunction(id, action){
             break;
     }
     ajaxRequest.send(null); 
-    //render();
+    // render();
     
 }
 
