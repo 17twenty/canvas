efeeec017066a757d42377fd3c4ae4ac0951bd9f
Ben Crundwell <bjc1@cambridge-design.co.uk>
2012-03-30 17:54:07 +0100

    Adding WEB content

--- src/backend-php/public_html/php/YouTube.php
+++ src/backend-php/public_html/php/YouTube.php
@@ -1,9 +1,11 @@
-<h1>YouTube Video Queue</h1><div style='float:right'><h1><a href="javascript:PopupMain();">Back</a></h1></div>
-<form action="" onsubmit="validateForm(event); return false;">
-<p>Please enter a URL to the YouTube you would like added to Canvas. For example "http://www.youtube.com/watch?v=QH2-TGUlwu4" 
-and then click Submit to start downloading the clip onto the Canvas server. <br/><br/>(Warning this can take some time especially for HD videos)</p><br/><br/>
-<label>YouTube URL: </label><input type='text' name='URL' size='50' />
-<button type="submit">Go!</button>
-</form>
-<br/><br/>
-<div id="YouTubeList"></div>
+<div id="menu">
+	<h1>Add YouTube Video</h1><div style='float:right'><h1><a href="javascript:PopupMain();">Back</a></h1></div>
+	<form action="" onsubmit="validateForm(event); return false;">
+	<p>Please enter a URL to the YouTube you would like added to Canvas. For example "http://www.youtube.com/watch?v=QH2-TGUlwu4" 
+	and then click Submit to start downloading the clip onto the Canvas server. <br/><br/>(Warning this can take some time especially for HD videos)</p><br/><br/>
+	<label>YouTube URL: </label><input type='text' name='URL' size='50' />
+	<button type="submit">Go!</button>
+	</form>
+	<br/><br/>
+	<div id="YouTubeList"></div>
+</div>
---
+++ src/backend-php/public_html/php/addWebLink.php
@@ -0,0 +1,10 @@
+<div id="menu">
+	<h1>Add Web Link</h1><div style='float:right'><h1><a href="javascript:PopupMain();">Back</a></h1></div>
+	<form action="" onsubmit="validateFormWeb(event); return false;">
+	<p>Please enter the URL to the web page you would like to add on Canvas</p><br/><br/>
+	<label>Web Page URL: </label><input type='text' name='URL' size='50' />
+	<button type="submit">Go!</button>
+	</form>
+	<br/><br/>
+	<div id="ExtraContent"></div>
+</div>
