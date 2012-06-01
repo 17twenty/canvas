b86f6a8701d1eb4626ed18a294f1a57b8229c255
Ben Crundwell <bjc1@cambridge-design.co.uk>
2012-06-01 09:28:20 +0100

    Repair config.php

--- src/backend-php/includes/config.php
+++ src/backend-php/includes/config.php
@@ -23,5 +23,5 @@
 
 // Make a MySQL Connection
 mysql_connect("localhost", "canvas", "password") or die(mysql_error("Database Connection Error"));
-mysql_select_db("canvasbeta") or die(mysql_error("Database Selection Error"));
+mysql_select_db("canvas") or die(mysql_error("Database Selection Error"));
 ?>
